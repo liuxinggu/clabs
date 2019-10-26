646e2c5a1375dd79af5aba5235ef97e548829ef8
LiuXinggu Name <1281242274@qq.com>
2019-10-26 00:45:44 -0700

    added a ctof.c file

--- lab1/ctof.c
+++ lab1/ctof.c
@@ -10,12 +10,10 @@ main()
     step = 5;
 
     celsius = lower;
-    printf ("celsius-fahr table\n")
+    printf ("celsius-fahr table\n");
     while (celsius <= upper)
     {fahr = celsius*9/5 + 32;
-    prinft("%d\t%d\n", celsius,fahr);
+    printf("%d\t%d\n", celsius,fahr);
     celsius = celsius + step;
-        /* code */
     }
-    
 }
