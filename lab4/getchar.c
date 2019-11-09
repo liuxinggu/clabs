3c36ca2e851fa4694a38b8d26b4da087f6a70287
LiuXinggu Name <1281242274@qq.com>
2019-11-09 07:09:14 -0800

    added a getchar.c file

--- lab4/getchar.c
+++ lab4/getchar.c
@@ -3,13 +3,12 @@ int main()
 {
     int count=0;
     char a;
-    int b=1;
     int c;
     while((a=getchar())!='\n'){
         c=c*10+(a-'0');}
-    while(b!=0){
-       b=b<<1;
-        if((b&c)!=0){count++;}
+    while(c!=0){
+        c=c&(c-1);
+        count++;
     }
 printf("%d\n",count);
 }
