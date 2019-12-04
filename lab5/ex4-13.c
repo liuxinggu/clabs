9f246aaf655712d553390ca301cd69b680af607a
LiuXinggu Name <1281242274@qq.com>
2019-12-04 06:57:57 -0800

    added a ex4-13 .c file

--- lab5/ex4-13.c
+++ lab5/ex4-13.c
@@ -1,26 +1,25 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 int main()
 {
     char s[100];
     char t[100];
-    int i=0;
-    
+    int i = 0;
+
     void reverse(char s[100]);
-    
-        while((s[i]=getchar())!='\n')
-    {i++;}
-    int j=i-1;
-    
-    int k=0;
-    while(j>=0){
-        t[k]=s[j];
+
+    while ((s[i] = getchar()) != '\n')
+    {
+        i++;
+    }
+    int j = i - 1;
+    int k = 0;
+    while (j >= 0)
+    {
+        t[k] = s[j];
         k++;
         j--;
     }
-    
-    
-    printf("%s\n",t);
+
+    printf("%s\n", t);
 }
-
-
